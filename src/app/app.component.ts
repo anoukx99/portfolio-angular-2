ad83b40a2b5ad9cf26a3dd4f164b7e6cb1de104e
degraaf <a.degraaf@aemics.nl>
2024-10-08 15:04:22 +0200

    Updated the portfolio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { bootstrapLinkedin, bootstrapGithub, bootstrapEnvelopeAtFill} from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, NgIconComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  viewProviders: [provideIcons({bootstrapLinkedin, bootstrapGithub, bootstrapEnvelopeAtFill})]
 })
 
 export class AppComponent {
-  title = 'portfolio-2';
+  title = 'Anouk de Graaf';
+
+  buttons = [ {
+    link: "https://www.linkedin.com/in/anouk-de-graaf",
+    icon: 'bootstrapLinkedin'
+  },
+  {
+    link: "https://github.com/anoukx99",
+    icon: 'bootstrapGithub'
+  }
+]
 }
